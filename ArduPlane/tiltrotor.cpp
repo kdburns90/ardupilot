28b0ce28b1a41a6ed5f0c1de23ac21434f73d6c0
Kevin Burns <kevin@aerhart.com>
2020-05-07 22:02:24 -0400

    test

--- ArduPlane/tiltrotor.cpp
+++ ArduPlane/tiltrotor.cpp
@@ -455,21 +455,13 @@ void QuadPlane::tiltrotor_vectored_yaw(void)
         SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorBackRight, 1000 * (base_output + elevon_right * elevon_range));
     } else { // hover
         float yaw_out = motors->get_yaw();
-        float pitch_out = 0; //motors->get_pitch();
         motors->set_yaw(0);
-        // motors->set_pitch(0);
         motors->output();
         float yaw_range = zero_out;
-
-        // angular velocity about yaw
-        // gyro_latest.z
-
-        // yaw stick position
-        // channel_yaw->get_control_in()
         
-        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorFrontLeft,  1000 * (base_output + yaw_out * yaw_range - pitch_out * yaw_range));
-        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorFrontRight, 1000 * (base_output - yaw_out * yaw_range - pitch_out * yaw_range));
-        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorBackLeft,  1000 * (base_output + yaw_out * yaw_range - pitch_out * yaw_range));
-        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorBackRight, 1000 * (base_output - yaw_out * yaw_range - pitch_out * yaw_range));
+        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorFrontLeft,  1000 * (base_output + yaw_out * yaw_range));
+        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorFrontRight, 1000 * (base_output - yaw_out * yaw_range));
+        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorBackLeft,  1000 * (base_output + yaw_out * yaw_range));
+        SRV_Channels::set_output_scaled(SRV_Channel::k_tiltMotorBackRight, 1000 * (base_output - yaw_out * yaw_range));
     }
 }
